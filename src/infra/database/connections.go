2044be4048b2f667ba69e0cca2521a567c04c8e4
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-21 12:14:01 +0100

    chore: :sparkles: Add connections

---
+++ src/infra/database/connections.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/manuelcunga/Omunga/src/modules/accounts/infra/entities"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init() *gorm.DB {
+
+	e := godotenv.Load() //Load .env file
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+
+	DSN := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
+	DB, err := gorm.Open(postgres.Open(DSN), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+
+	}
+
+	fmt.Println("Connection Opened to Database")
+
+	DB.AutoMigrate(entities.User{})
+
+	log.Println("Connected to database...")
+
+	return DB
+}
