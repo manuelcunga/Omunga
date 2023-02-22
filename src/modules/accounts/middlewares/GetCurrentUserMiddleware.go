e3a62a43fa96e4b8f641e7d2b256cf6980a07d3b
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-22 10:37:09 +0100

    feat: :sparkles: adicionando getcurrent user

---
+++ src/modules/accounts/middlewares/GetCurrentUserMiddleware.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/manuelcunga/Omunga/src/modules/accounts/infra/entities"
+	repositories "github.com/manuelcunga/Omunga/src/modules/accounts/repositories/interfaces"
+)
+
+type CreateUserRepository struct {
+	userRepository repositories.ICreateUserRepository
+}
+
+func (userRepo *CreateUserRepository) GetCurrentUser(w http.ResponseWriter, r *http.Request) (*entities.User, error) {
+	// Obter o ID do usuário atual a partir do contexto da requisição
+	userID, ok := r.Context().Value("userID").(string)
+	if !ok {
+		return nil, fmt.Errorf("User ID not found in context")
+	}
+
+	// Buscar as informações completas do usuário correspondente no banco de dados
+	user, err := userRepo.userRepository.FindById(userID)
+	if err != nil {
+		return err, nil
+	}
+
+	return nil, user
+}
