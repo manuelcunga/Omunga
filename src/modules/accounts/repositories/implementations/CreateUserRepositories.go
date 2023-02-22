ebc623421867c6ed55c4de2c9a1138c2e7af166f
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-22 10:38:14 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/repositories/implementations/CreateUserRepositories.go
+++ src/modules/accounts/repositories/implementations/CreateUserRepositories.go
@@ -1,23 +1,25 @@
 package repositories
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/manuelcunga/Omunga/src/modules/accounts/infra/entities"
 	repositories "github.com/manuelcunga/Omunga/src/modules/accounts/repositories/interfaces"
 	"gorm.io/gorm"
 )
 
-type userRepository struct {
+type UserRepository struct {
 	Db *gorm.DB
 }
 
 func NewUserRepository(db *gorm.DB) repositories.ICreateUserRepository {
-	return &userRepository{
+	return &UserRepository{
 		Db: db,
 	}
 }
 
-// Create implements repositories.ICreateUserRepository
-func (user *userRepository) Create(data *entities.User) error {
+func (user *UserRepository) Create(data *entities.User) error {
 	err := user.Db.Create(&data).Error
 
 	if err != nil {
@@ -27,8 +29,7 @@ func (user *userRepository) Create(data *entities.User) error {
 	return nil
 }
 
-// FindAll implements repositories.ICreateUserRepository
-func (users *userRepository) FindAll() ([]*entities.User, error) {
+func (users *UserRepository) FindAll() ([]*entities.User, error) {
 	var allUsers []*entities.User
 
 	err := users.Db.Find(&allUsers).Error
@@ -38,4 +39,51 @@ func (users *userRepository) FindAll() ([]*entities.User, error) {
 	}
 
 	return allUsers, nil
+}
+
+func (userRepo *UserRepository) FindByEmail(email string) (*entities.User, error) {
+
+	user := &entities.User{}
+
+	result := userRepo.Db.Where("email = ?", email).First(user)
+
+	if result.Error != nil {
+
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+
+			return nil, fmt.Errorf("Ups, usuário não encontrado '%s' not found", email)
+		}
+		return nil, result.Error
+	}
+	return user, nil
+}
+
+func (userRepo *UserRepository) Delete(id string) (error, *entities.User) {
+
+	user := &entities.User{}
+	result := userRepo.Db.Where("id = ?", id).Delete(user)
+	if result.Error != nil {
+		return result.Error, nil
+	}
+	return nil, user
+}
+
+func (userRepo *UserRepository) FindById(id string) (error, *entities.User) {
+	user := &entities.User{}
+	result := userRepo.Db.Where("id = ?", id).First(user)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("user with id '%s' not found", id), nil
+		}
+		return result.Error, nil
+	}
+	return nil, user
+}
+
+func (userRepo *UserRepository) Update(user *entities.User) (error, *entities.User) {
+	result := userRepo.Db.Save(user)
+	if result.Error != nil {
+		return result.Error, nil
+	}
+	return nil, user
 }
