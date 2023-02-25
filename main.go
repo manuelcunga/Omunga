822bb0d41370bbf05734b1a0e3ba586928512b3c
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-25 20:48:50 +0100

    refactor: :recycle: refatorando

--- main.go
+++ main.go
@@ -6,12 +6,9 @@ import (
 )
 
 func main() {
-	// database.Connection()
 	app := echo.New()
 	routes.Setup(app)
 
 	app.Start(":4002")
-	// if err := app.Start(":4002"); err != nil {
-	// 	log.Fatalln("Failed to start server:", err)
-	// }
+
 }
