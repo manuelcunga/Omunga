7c12f6c291bdfbc6aaa8d2c710df2d6359a32e31
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-25 20:49:54 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/infra/entities/Base.go
+++ src/modules/accounts/infra/entities/Base.go
@@ -10,6 +10,13 @@ func init() {
 	govalidator.SetFieldsRequiredByDefault(true)
 }
 
+// func (Base) init() {
+// 	govalidator.CustomTypeTagMap.Set("time.Time", govalidator.CustomTypeValidator(func(i interface{}, context interface{}) bool {
+// 		_, ok := i.(time.Time)
+// 		return ok
+// 	}))
+// }
+
 type Base struct {
 	CreatedAt time.Time `gorm:"autoCreateTime:true" json:"created_at" valid:"-"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime:true" json:"updated_at" valid:"-"`
