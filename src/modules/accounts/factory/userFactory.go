30c582912bd7a67593597cdb28f1b7e95a37308a
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-25 20:49:36 +0100

    feat: :sparkles: adicionando instacias de listagem de usuarios

--- src/modules/accounts/factory/userFactory.go
+++ src/modules/accounts/factory/userFactory.go
@@ -4,9 +4,20 @@ import (
 	"github.com/manuelcunga/Omunga/src/infra/database"
 	repositories "github.com/manuelcunga/Omunga/src/modules/accounts/repositories/implementations"
 	usecases "github.com/manuelcunga/Omunga/src/modules/accounts/usecases/create"
+	findAllUsers "github.com/manuelcunga/Omunga/src/modules/accounts/usecases/findAll"
 )
 
 func NewUserUseCase() usecases.CreateUserUseCases {
 	repo := repositories.NewUserRepository(database.Connection())
 	return usecases.NewUserUseCase(repo)
+}
+
+func NewListUsersUseCase() findAllUsers.FindAllUsersUseCases {
+	repo := repositories.NewUserRepository(database.Connection())
+	return findAllUsers.NewFindAllUsersUseCases(repo)
+}
+
+func NewListUsersUseCases() findAllUsers.FindAllUsersUseCases {
+	repo := repositories.NewUserRepository(database.Connection())
+	return findAllUsers.NewFindAllUsersUseCases(repo)
 }
