1a210b4ec058673c4f722590f80b829394f2e244
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-25 20:50:52 +0100

    refactor: :recycle: adicionando routa para listar usuarios

--- src/routes/user.routes.go
+++ src/routes/user.routes.go
@@ -3,11 +3,17 @@ package routes
 import (
 	"github.com/labstack/echo"
 	controller "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/create"
+	usersController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/findAll"
 	"github.com/manuelcunga/Omunga/src/modules/accounts/factory"
 )
 
 func UserRoutes(e *echo.Group) {
 	userFactory := factory.NewUserUseCase()
 	createUserController := controller.NewUserController(userFactory)
+
+	listUsersFactory := factory.NewListUsersUseCase()
+	listUsersUseCaseController := usersController.NewUsersController(listUsersFactory)
+
 	e.POST("/user/create", createUserController.CreateUser())
+	e.GET("/users", listUsersUseCaseController.FindAllUsers())
 }
