f58218b03eb4c80c5eb02602adbed3cb888ea596
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-27 22:38:38 +0100

    feat: :sparkles: Adicioando usecase para login

--- src/modules/accounts/usecases/login/loginUseCases.go
+++ src/modules/accounts/usecases/login/loginUseCases.go
@@ -6,12 +6,12 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt"
-	"github.com/manuelcunga/Omunga/src/modules/accounts/infra/entities"
+	"github.com/manuelcunga/Omunga/src/modules/accounts/dtos"
 	repositories "github.com/manuelcunga/Omunga/src/modules/accounts/repositories/interfaces"
 	"golang.org/x/crypto/bcrypt"
 )
 
-type CreateUserRepository struct {
+type LoginUseCases struct {
 	userRepository repositories.ICreateUserRepository
 }
 
@@ -22,23 +22,25 @@ type UserClaims struct {
 	jwt.StandardClaims
 }
 
-func (userRepo *CreateUserRepository) Login(email, password string) (string, error) {
-	user := &entities.User{}
+func NewLoginUseCases(userRepo repositories.ICreateUserRepository) LoginUseCases {
+	return LoginUseCases{userRepository: userRepo}
+}
 
-	_, err := userRepo.userRepository.FindByEmail(email)
+func (loginUseCases *LoginUseCases) Login(data *dtos.LoginDTO) (string, error) {
+	user, err := loginUseCases.userRepository.FindByEmail(data.Email)
 
 	if err != nil {
 		return "", fmt.Errorf("Email ou palavra passe errada...")
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data.Password)); err != nil {
 		return "", fmt.Errorf("Email ou palavra passe errada..., ")
 	}
 
 	expirationTime := time.Now().Add(24 * time.Hour)
 	claims := &UserClaims{
 		ID:        user.ID,
-		Email:     email,
+		Email:     data.Email,
 		ExpiresAt: expirationTime.Unix(),
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expirationTime.Unix(),
@@ -47,7 +49,7 @@ func (userRepo *CreateUserRepository) Login(email, password string) (string, err
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	jwtKey := os.Getenv("JWT_SECRET")
-	tokenString, err := token.SignedString(jwtKey)
+	tokenString, err := token.SignedString([]byte(jwtKey))
 
 	if err != nil {
 		return "", fmt.Errorf("failed to generate JWT token: %v", err)
