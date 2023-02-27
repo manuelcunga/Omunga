8c252a3a301100850ac6fd1aacd111c7fb1a48c1
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-27 22:38:56 +0100

    refactor: :recycle: refatorando rotas

--- src/routes/user.routes.go
+++ src/routes/user.routes.go
@@ -3,10 +3,13 @@ package routes
 import (
 	"github.com/labstack/echo"
 	controller "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/create"
+	deleteUserController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/delete"
 	usersController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/findAll"
 	findOneUserController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/findOne"
+	loginController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/login"
 	updateUserController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/update"
 	"github.com/manuelcunga/Omunga/src/modules/accounts/factory"
+	"github.com/manuelcunga/Omunga/src/modules/accounts/middlewares"
 )
 
 func UserRoutes(e *echo.Group) {
@@ -22,8 +25,18 @@ func UserRoutes(e *echo.Group) {
 	udateUserFactory := factory.NewUpdateUserUsecases()
 	updateUser := updateUserController.NewUpdateUserController(udateUserFactory)
 
+	deleteUserFactory := factory.NewDeleteUserUseCases()
+	deleteUser := deleteUserController.NewDeleteUserController(deleteUserFactory)
+
+	loginFactory := factory.NewLoginUseCases()
+	loginUser := loginController.NewLoginController(loginFactory)
+
+	e.POST("/auth/login", loginUser.Login())
 	e.POST("/user/create", createUserController.CreateUser())
+
+	e.Use(middlewares.AuthMiddlewareFunc())
 	e.GET("/users", listUsersUseCaseController.FindAllUsers())
 	e.GET("/user/:id", findOneUserController.FindOneUser())
 	e.PATCH("/user/:id", updateUser.UpdateUser())
+	e.DELETE("/user/:id", deleteUser.DeleteUser())
 }
