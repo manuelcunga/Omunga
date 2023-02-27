8aedc1fdbd48e03f03e6a371657b4b0b68f25129
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-27 12:00:42 +0100

    refactor: :recycle: adicionando rota para pegar unico usuario e atualizar usuario

--- src/routes/user.routes.go
+++ src/routes/user.routes.go
@@ -4,6 +4,8 @@ import (
 	"github.com/labstack/echo"
 	controller "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/create"
 	usersController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/findAll"
+	findOneUserController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/findOne"
+	updateUserController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/update"
 	"github.com/manuelcunga/Omunga/src/modules/accounts/factory"
 )
 
@@ -14,6 +16,14 @@ func UserRoutes(e *echo.Group) {
 	listUsersFactory := factory.NewListUsersUseCase()
 	listUsersUseCaseController := usersController.NewUsersController(listUsersFactory)
 
+	findOneuserFactory := factory.NewFindOneUserUseCases()
+	findOneUserController := findOneUserController.NewFindOneUserController(findOneuserFactory)
+
+	udateUserFactory := factory.NewUpdateUserUsecases()
+	updateUser := updateUserController.NewUpdateUserController(udateUserFactory)
+
 	e.POST("/user/create", createUserController.CreateUser())
 	e.GET("/users", listUsersUseCaseController.FindAllUsers())
+	e.GET("/user/:id", findOneUserController.FindOneUser())
+	e.PATCH("/user/:id", updateUser.UpdateUser())
 }
