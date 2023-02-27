d878aab1a521a040cf2094b3ebdb9a87e2b63179
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-27 11:58:36 +0100

    refactor: :recycle: adicionando factory de atualização de usuário

--- src/modules/accounts/factory/userFactory.go
+++ src/modules/accounts/factory/userFactory.go
@@ -5,6 +5,8 @@ import (
 	repositories "github.com/manuelcunga/Omunga/src/modules/accounts/repositories/implementations"
 	usecases "github.com/manuelcunga/Omunga/src/modules/accounts/usecases/create"
 	findAllUsers "github.com/manuelcunga/Omunga/src/modules/accounts/usecases/findAll"
+	findOneUsers "github.com/manuelcunga/Omunga/src/modules/accounts/usecases/findOne"
+	updateUser "github.com/manuelcunga/Omunga/src/modules/accounts/usecases/update"
 )
 
 func NewUserUseCase() usecases.CreateUserUseCases {
@@ -20,4 +22,14 @@ func NewListUsersUseCase() findAllUsers.FindAllUsersUseCases {
 func NewListUsersUseCases() findAllUsers.FindAllUsersUseCases {
 	repo := repositories.NewUserRepository(database.Connection())
 	return findAllUsers.NewFindAllUsersUseCases(repo)
+}
+
+func NewFindOneUserUseCases() findOneUsers.FindOndeUserUseCases {
+	repo := repositories.NewUserRepository(database.Connection())
+	return findOneUsers.NewFindOndeUserUseCases(repo)
+}
+
+func NewUpdateUserUsecases() updateUser.UpdateUserUseCases {
+	repo := repositories.NewUserRepository(database.Connection())
+	return updateUser.NewUpdateUserUseCases(repo)
 }
