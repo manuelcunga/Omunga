dc100ffeb452433fd0e80f3e46082e9a3ab4ceaa
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-27 12:00:00 +0100

    feat: :sparkles: Adicioando caso de uso para pegar um unico usuario

---
+++ src/modules/accounts/usecases/findOne/findOneUserUseCase.go
@@ -0,0 +1,22 @@
+package usecases
+
+import (
+	"github.com/manuelcunga/Omunga/src/modules/accounts/infra/entities"
+	repositories "github.com/manuelcunga/Omunga/src/modules/accounts/repositories/interfaces"
+)
+
+type FindOndeUserUseCases struct {
+	userRepository repositories.ICreateUserRepository
+}
+
+func NewFindOndeUserUseCases(userRepo repositories.ICreateUserRepository) FindOndeUserUseCases {
+	return FindOndeUserUseCases{userRepository: userRepo}
+}
+
+func (user *FindOndeUserUseCases) FindOne(userId string) (*entities.User, error) {
+	foundUser, err := user.userRepository.FindById(userId)
+	if err != nil {
+		return nil, err
+	}
+	return foundUser, nil
+}
