e9f4c8f63a9d070424d56766ff074d95f79e56e6
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-03-01 14:09:48 +0100

    feat: :sparkles:  user usecases

--- src/routes/user.routes.go
+++ src/routes/user.routes.go
@@ -7,6 +7,8 @@ import (
 	usersController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/findAll"
 	findOneUserController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/findOne"
 	loginController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/login"
+	meController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/me"
+	userProfileController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/profile"
 	updateUserController "github.com/manuelcunga/Omunga/src/modules/accounts/controllers/update"
 	"github.com/manuelcunga/Omunga/src/modules/accounts/factory"
 	"github.com/manuelcunga/Omunga/src/modules/accounts/middlewares"
@@ -31,6 +33,12 @@ func UserRoutes(e *echo.Group) {
 	loginFactory := factory.NewLoginUseCases()
 	loginUser := loginController.NewLoginController(loginFactory)
 
+	profileFactory := factory.NewProfileUseCases()
+	profile := userProfileController.NewProfileController(profileFactory)
+
+	meFactory := factory.NewMeUseCases()
+	meUserController := meController.NewMeController(meFactory)
+
 	e.POST("/auth/login", loginUser.Login())
 	e.POST("/user/create", createUserController.CreateUser())
 
@@ -39,4 +47,7 @@ func UserRoutes(e *echo.Group) {
 	e.GET("/user/:id", findOneUserController.FindOneUser())
 	e.PATCH("/user/:id", updateUser.UpdateUser())
 	e.DELETE("/user/:id", deleteUser.DeleteUser())
+	e.GET("/user/profile/:id", profile.Profile())
+	e.GET("/user/me", echo.HandlerFunc(meUserController.Me), middlewares.Me())
+
 }
